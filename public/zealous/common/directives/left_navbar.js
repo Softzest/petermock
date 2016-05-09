04661d036c5af34118baf0cc3328650794768efc
hardeyem <hardeyem111@gmail.com>
2016-05-09 22:16:25 +0100

    Initial commit for frontend team to work with

---
+++ public/zealous/common/directives/left_navbar.js
@@ -0,0 +1,19 @@
+/**
+ * Created by TK on 13/02/2016.
+ */
+
+(function(window, angular, undefined) {
+    'use strict';
+
+    angular
+        .module('MainDirective')
+        .directive('qpLeftNavbar', [function () {
+            return {
+                scope: {},
+                restrict: 'A',
+                templateUrl: 'partials/left_navbar.html',
+                controller : 'NavbarCtrl as navbar'
+
+            };
+        }]);
+})(window, window.angular);
